4a0aa8769f706c6cbbb701df89201f2927cb9ba1
Jonathan Gruss <jonathangruss@hotmail.com>
2022-12-14 21:14:04 +0100

    retro collaborator page

--- src/pages/Collaborators.tsx
+++ src/pages/Collaborators.tsx
@@ -14,14 +14,16 @@ const Collaborators = () => {
   return (
     <div className='h-full flex flex-col items-center justify-center gap-4'>
       <div className='max-w-sm'>
-        <Card>
-          <div className='mb-4 flex items-center justify-between'>
-            <h5 className='text-xl font-bold leading-none text-gray-900 dark:text-white'>
-              Collaborators
-            </h5>
-          </div>
-          <div className='flow-root'>
-            <ul className='divide-y divide-gray-200 dark:divide-gray-700'>
+        <div className='card w-[400px] p-0 divide-y-2 divide-black overflow-hidden'>
+          <div className='h-7 bg-accent' />
+
+          <div className='p-4'>
+            <div className='my-4 flex items-center justify-between'>
+              <h5 className='text-xl font-bold leading-none text-black'>
+                Collaborators
+              </h5>
+            </div>
+            <ul className='divide-y divide-black/25 '>
               {getCollaborators().map((collaborator, i) => {
                 return (
                   <UserItem
@@ -32,50 +34,53 @@ const Collaborators = () => {
               })}
             </ul>
           </div>
-          <div className='mt-4 flex items-center justify-between'>
-            <h5 className='text-xl font-bold leading-none text-gray-900 dark:text-white'>
-              Access Control
-            </h5>
+
+          <div className='w-full my-4 p-4 flex flex-col justify-between'>
+            <div className='mb-4 flex items-center justify-between'>
+              <h5 className='text-xl font-bold leading-none text-black'>
+                Access Control
+              </h5>
+            </div>
+
+            <form className='flex flex-col gap-4'>
+              <div>
+                <TextInput
+                  id='provider'
+                  type='text'
+                  value={webId}
+                  placeholder='WebId'
+                  onChange={(e) => setWebId(e.target.value)}
+                  required={true}
+                  className='border-main'
+                />
+              </div>
+              <div className='flex flex-row gap-4 w-full justify-between'>
+                <div className=''>
+                  <div
+                    className='btn btn-primary'
+                    onClick={() => {
+                      addWriteAccess(webId);
+                      setWebId('');
+                    }}
+                  >
+                    Add Write Access
+                  </div>
+                </div>
+                <div className=''>
+                  <div
+                    className='btn btn-primary'
+                    onClick={() => {
+                      addReadAccess(webId);
+                      setWebId('');
+                    }}
+                  >
+                    Add Read Access
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
-          <form className='flex flex-col gap-4'>
-            <div>
-              <TextInput
-                id='provider'
-                type='text'
-                value={webId}
-                placeholder='WebId'
-                onChange={(e) => setWebId(e.target.value)}
-                required={true}
-              />
-            </div>
-            <div className='flex flex-row gap-4 w-full items-center justify-between'>
-              <div className=''>
-                <Button
-                  gradientDuoTone='purpleToBlue'
-                  outline={true}
-                  onClick={() => {
-                    addWriteAccess(webId);
-                    setWebId('');
-                  }}
-                >
-                  Add Write Access
-                </Button>
-              </div>
-              <div className=''>
-                <Button
-                  gradientDuoTone='purpleToBlue'
-                  outline={true}
-                  onClick={() => {
-                    addReadAccess(webId);
-                    setWebId('');
-                  }}
-                >
-                  Add Read Access
-                </Button>
-              </div>
-            </div>
-          </form>
-        </Card>
+        </div>
       </div>
     </div>
   );
@@ -87,20 +92,20 @@ interface UserCardProps {
 
 const UserItem = (props: UserCardProps) => {
   return (
-    <li className='py-3 sm:py-4'>
+    <li className='py-3'>
       <div className='flex items-center space-x-4'>
         <div className='shrink-0'>
           <img
-            className='h-8 w-8 rounded-full'
+            className='h-8 w-8 rounded-full border-main shadow-full_sm'
             src='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png'
             alt='User'
           />
         </div>
         <div className='min-w-0 flex-1'>
-          <p className='truncate text-sm font-bold text-zinc-900 dark:text-white'>
+          <p className='truncate text-sm font-bold text-black'>
             {props.collaborator.webId}
           </p>
-          <p className='truncate text-sm text-zinc-500 dark:text-zinc-400'>
+          <p className='truncate text-sm font-bold text-secondary'>
             {props.collaborator.isCreator ? 'Creator' : 'Contributor'}
           </p>
         </div>
