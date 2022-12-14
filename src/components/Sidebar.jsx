08a6758ef606db213da61b823e155bec3372f276
Jonathan Gruss <jonathangruss@hotmail.com>
2022-12-14 20:47:28 +0100

    retro sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,10 +5,46 @@ import {
   UserGroupIcon,
 } from '@heroicons/react/24/outline';
 
+import { BiHomeAlt, BiUser, BiWorld } from 'react-icons/bi';
+
 import { Sidebar } from 'flowbite-react';
 import { Link } from 'react-router-dom';
 
 const MySidebar = (props) => {
+  return (
+    <div className={props.className}>
+      <div className='flex flex-col w-16 h-full bg-neutral items-center'>
+        <div className='flex-0 mt-2'>
+          <img src='logo512.png' alt='logo' className='h-12 w-12' />
+        </div>
+
+        <div className='flex grow flex-col items-center justify-center gap-2'>
+          <div className='m-3 border-main bg-secondary hover:bg-secondary-dark shadow-full_sm'>
+            <Link to='/'>
+              <BiHomeAlt className='h-8 w-8 text-black' />
+            </Link>
+          </div>
+          <div className='m-3 border-main bg-accent hover:bg-accent-dark shadow-full_sm'>
+            <Link to='/collaborators'>
+              <BiWorld className='h-8 w-8 text-black' />
+            </Link>
+          </div>
+          <div className='m-3 border-main bg-primary hover:bg-primary-dark shadow-full_sm'>
+            <Link to='/profile'>
+              <BiUser className='h-8 w-8 text-black' />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Item = (props) => {
+  return <div className=''></div>;
+};
+
+const MySidebar2 = (props) => {
   return (
     <Sidebar
       aria-label='Default sidebar example'
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -5,7 +5,7 @@ import MySidebar from '../components/Sidebar';
 const Layout = () => {
   return (
     <div className='flex flex-row w-screen h-screen bg-main'>
-      <MySidebar className='m-2 shadow-xl ' />
+      <MySidebar className='border-r-2 border-black' />
       <div className='grow h-screen overflow-auto'>
         <Outlet />
       </div>
